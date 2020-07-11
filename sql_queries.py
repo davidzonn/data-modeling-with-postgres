6098130e0afa2c1fa73c4747464c8ab1c5b99e02
davidzonn <davidzonneveld@gmail.com>
2020-07-11 23:59:28 +0200

    add parameterized insertion queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,9 +1,9 @@
 # DROP TABLES
 
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
-user_table_drop = "DROP TABLE IF EXISTS \"user\""
-song_table_drop = "DROP TABLE IF EXISTS song"
-artist_table_drop = "DROP TABLE IF EXISTS artist"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
@@ -23,7 +23,7 @@ songplay_table_create = ("""
 """)
 
 user_table_create = ("""
-    CREATE TABLE IF NOT EXISTS \"user\" (
+    CREATE TABLE IF NOT EXISTS users (
       id INT PRIMARY KEY,
       first_name VARCHAR,
       last_name VARCHAR,
@@ -33,7 +33,7 @@ user_table_create = ("""
 """)
 
 song_table_create = ("""
-    CREATE TABLE IF NOT EXISTS song (
+    CREATE TABLE IF NOT EXISTS songs (
       id VARCHAR PRIMARY KEY,
       title VARCHAR,
       artist_id VARCHAR,
@@ -43,7 +43,7 @@ song_table_create = ("""
 """)
 
 artist_table_create = ("""
-    CREATE TABLE IF NOT EXISTS artist (
+    CREATE TABLE IF NOT EXISTS artists (
       id VARCHAR PRIMARY KEY,
       name VARCHAR,
       location VARCHAR,
@@ -67,19 +67,29 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays ( id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent )
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+    INSERT INTO users ( id, first_name, last_name, gender, level )
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs ( id, title, artist_id, year, duration )
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists ( id, name, location, latitude, longitude )
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time ( start_time, hour, day, week, month, year, weekday )
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
@@ -88,6 +98,7 @@ song_select = ("""
 """)
 
 # QUERY LISTS
+
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
