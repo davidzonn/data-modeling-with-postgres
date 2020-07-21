fd7c62fe7a630e7bf0827a11f1f6e609c885bd7b
davidzonn <davidzonneveld@gmail.com>
2020-07-21 23:45:51 +0200

    implement etl pipeline

--- sql_queries.py
+++ sql_queries.py
@@ -10,8 +10,8 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
-      id INT PRIMARY KEY,
-      start_time TIMESTAMP,
+      id SERIAL PRIMARY KEY,
+      start_time BIGINT,
       user_id INT,
       level VARCHAR,
       song_id VARCHAR,
@@ -67,34 +67,41 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-    INSERT INTO songplays ( id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent )
-    VALUES (%s, %s, %s, %s, %s)
+    INSERT INTO songplays ( start_time, user_id, level, song_id, artist_id, session_id, location, user_agent )
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
     INSERT INTO users ( id, first_name, last_name, gender, level )
     VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""
     INSERT INTO songs ( id, title, artist_id, year, duration )
     VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""
     INSERT INTO artists ( id, name, location, latitude, longitude )
     VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
 """)
 
 
 time_table_insert = ("""
     INSERT INTO time ( start_time, hour, day, week, month, year, weekday )
     VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT songs.id as songid, artists.id as artistid
+    FROM songs JOIN artists ON songs.artist_id = artists.id
+    WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
 """)
 
 # QUERY LISTS
